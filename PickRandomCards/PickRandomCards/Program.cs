9cc133c2fb059272b1fc09a85d9f1a90c98cd706
ABWeb-Dev <93297010+ABWeb-Dev@users.noreply.github.com>
2021-11-12 15:50:43 -0800

    Add files via upload

---
+++ PickRandomCards/PickRandomCards/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PickRandomCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of cards to pick: ");
+
+            string line = Console.ReadLine();
+
+            if (int.TryParse(line, out int numberofCards))
+            {
+               string[] cardsPicked = CardPicker.PickSomeCards(numberofCards);
+                foreach(string card in cardsPicked)
+                {
+                    Console.WriteLine(card);
+                }
+            } else
+            {
+                Console.WriteLine("Please enter a valid, positive, integer.");
+            }
+        }
+    }
+}
